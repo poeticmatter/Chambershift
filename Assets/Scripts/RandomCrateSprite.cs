9fdcc8e9f3b3e782fe48c861666fa4c2a91b185c
Itai <d00m.poet@gmail.com>
2016-04-17 08:44:32 +0300

    basic platformer movement

---
+++ Assets/Scripts/RandomCrateSprite.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomCrateSprite : MonoBehaviour {
+	public Sprite[] sprites;
+	
+	void Start () {
+		GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+	}
+	
+	
+}
